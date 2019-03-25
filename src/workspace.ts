e0fa4016d9628c36763e3c05ce7ae216fc7168f1
Bina Cohen <46712931+BinaCohen@users.noreply.github.com>
2019-03-25 14:57:11 +0200

    Add employees routes and some other changes

--- src/workspace.ts
+++ src/workspace.ts
@@ -1,26 +1,28 @@
-import { Node, NodeType } from "./node";
+import { Node, NodeType, Route } from "./node";
 
 export class Workspace {
     private workspacePlan: string[][] = new Array();
     private employeesIndexes: [number, number][] = new Array();
     private nodesMatrix: Node[][] = new Array();
-    private nodesMap: Map<number, Node> = new Map();
+    private spaceNodesMap: Map<number, Node> = new Map();
     kitchenLocation: [number, number];
 
     constructor(input: string) {
         this.parseWorkspacePlanInput(input);
         this.saveWorkersIndexes();
         this.initNodesMatrix();
-        this.initNodeMap();
+        this.initSpacesNodeMap();
     }
-//Divide the string containing the file content into a matrix of characters
+
+    // Parse the file input into a matrix of characters
     private parseWorkspacePlanInput(input: string): void {
         const lines: string[] = input.split("\n");
         for (const line of lines) {
             this.workspacePlan.push(line.split(""));
         }
     }
-//Save workers indexes
+
+    // Save workers indexes
     private saveWorkersIndexes() {
         for (let i = 0; i < this.workspacePlan.length; i++) {
             for (let j = 0; j < this.workspacePlan[i].length; j++) {
@@ -30,13 +32,15 @@ export class Workspace {
             }
         }
     }
-//Print workspace plan
+
+    // Print workspace plan
     printWorkspacePlan(): void {
         for (const row of this.workspacePlan) {
             console.log(row.join(""));
         }
     }
-//Create a nodes matrix that save each element from the original matrix and adds an ID number 
+
+    // Create a nodes matrix which contains each element from the original matrix with additional data
     private initNodesMatrix(): void {
         let id = 0;
         for (let i = 0; i < this.workspacePlan.length; i++) {
@@ -48,19 +52,21 @@ export class Workspace {
             this.nodesMatrix.push(nodesRow);
         }
     }
-//Create a map and save all the spaces in the matrix by ID
-    private initNodeMap(): void {
+
+    // Create a map contains all the space nodes in the matrix
+    private initSpacesNodeMap(): void {
         for (let i = 0; i < this.nodesMatrix.length; i++) {
             for (let j = 0; j < this.nodesMatrix[i].length; j++) {
                 const node = this.nodesMatrix[i][j];
                 if (node.type === NodeType.SPACE) {
-                    this.nodesMap.set(node.id, node);
+                    this.spaceNodesMap.set(node.id, node);
                     this.setNodeConnections(node);
                 }
             }
         }
     }
-//For each of the spaces in the map save all spaces neighbors
+
+    // Link all neighbors space nodes for a given node
     private setNodeConnections(node: Node): void {
         const [i, j] = node.index;
         const neighborsNodes: Node[] = new Array();
@@ -75,49 +81,81 @@ export class Workspace {
         }
     }
 
-    //For each of the empty spaces save distance - the shortest path from the employee to the empty space (The amount of nodes).
-    //For each of the empty space sum the distances from all employees to the empty space.
-    //Implementing an Dijkstra algorithm for finding the distance.
+    // Update the workspace plan with kitchen location and routes from the employees
     setKitchenLocation(): void {
-        for (const employeeIndex of this.employeesIndexes) {
-            const [i, j] = employeeIndex;
-            const employeNode = this.nodesMatrix[i][j];
-            employeNode.distance = 0;
-            this.setNodeConnections(employeNode);
-            this.nodesMap.set(employeNode.id, employeNode);
-            const nonVisitedNodes = Array.from(this.nodesMap.values());
-            while (nonVisitedNodes.length > 0) {
-                const minDistanceNode = nonVisitedNodes
-                    .reduce((minDistanceNode, node) => node.distance < minDistanceNode.distance ? node : minDistanceNode, nonVisitedNodes[0]);
-                for (const connectedNode of minDistanceNode.connectedNodes) {
-                    if (!connectedNode.visited) {
-                        const newDistabce = minDistanceNode.distance + 1;
-                        if (newDistabce < connectedNode.distance) {
-                            connectedNode.distance = newDistabce;
-                            connectedNode.parent = minDistanceNode;
-                        }
-                    }
-                }
-                minDistanceNode.visited = true;
-                nonVisitedNodes.splice(nonVisitedNodes.indexOf(minDistanceNode), 1);
-            }
-            for (const node of this.nodesMap.values()) {
-                node.sumOfDistances += node.distance;
-                node.visited = false;
-                node.distance = Infinity;
-                node.parent = null;
-            }
-        }
-        const nodesArray = Array.from(this.nodesMap.values());
-        //Finding the empty sapce with the minimum amount of distances.
-        const kitchenNode = nodesArray.filter(node => node.type === NodeType.SPACE)
-            .reduce((kitchenNode, node) => node.sumOfDistances < kitchenNode.sumOfDistances ? node : kitchenNode, nodesArray[0]);
-        //If the floor plan does not allow for a kitchen
-        if (kitchenNode.sumOfDistances === Infinity) {
-            throw new Error("There is no space accessible for all employees!");
-        }
+        const kitchenNode = this.findKitchenNode();
         this.kitchenLocation = kitchenNode.index;
         const [i, j] = kitchenNode.index;
         this.workspacePlan[i][j] = "*";
+        for (const employeeRoute of kitchenNode.employeesRoutes) {
+            for (const index of employeeRoute) {
+                const [i, j] = index;
+                if (this.nodesMatrix[i][j].type === NodeType.SPACE) {
+                    this.workspacePlan[i][j] = "+";
+                }
+            }
+        }
+    }
+
+    // For each employee find the shortest path to all of the space nodes
+    // For each space node wee will keep (as part of Node object) the sum of distances from all employees
+    private findKitchenNode(): Node {
+        for (const employeeIndex of this.employeesIndexes) {
+            const [i, j] = employeeIndex;
+            const employeNode = this.nodesMatrix[i][j];
+            this.findNodeShortestPaths(employeNode);
+        }
+        const nodesArray = Array.from(this.spaceNodesMap.values());
+        // Find the empty sapce with the minimum distance 
+        const kitchenNode = nodesArray.filter(node => node.type === NodeType.SPACE)
+            .reduce((kitchenNode, node) => node.sumOfDistances < kitchenNode.sumOfDistances ? node : kitchenNode, nodesArray[0]);
+        // For case the floor plan does not have a space accessible for all employees
+        if (kitchenNode.sumOfDistances === Infinity) {
+            throw new Error("There is no space accessible for all employees!");
+        }
+        return kitchenNode;
+    }
+
+    // Dijkstra algorithm
+    private findNodeShortestPaths(employeNode: Node) {
+        employeNode.distance = 0;
+        this.setNodeConnections(employeNode);
+        this.spaceNodesMap.set(employeNode.id, employeNode);
+        const nonVisitedNodes = Array.from(this.spaceNodesMap.values());
+        while (nonVisitedNodes.length > 0) {
+            const minDistanceNode = nonVisitedNodes
+                .reduce((minDistanceNode, node) => node.distance < minDistanceNode.distance ? node : minDistanceNode, nonVisitedNodes[0]);
+            for (const connectedNode of minDistanceNode.connectedNodes) {
+                if (!connectedNode.visited) {
+                    const newDistabce = minDistanceNode.distance + 1;
+                    if (newDistabce < connectedNode.distance) {
+                        connectedNode.distance = newDistabce;
+                        connectedNode.parent = minDistanceNode;
+                    }
+                }
+            }
+            minDistanceNode.visited = true;
+            nonVisitedNodes.splice(nonVisitedNodes.indexOf(minDistanceNode), 1);
+        }
+        for (const node of this.spaceNodesMap.values()) {
+            node.sumOfDistances += node.distance;
+            this.saveEmployeeRoute(node);
+        }
+        for (const node of this.spaceNodesMap.values()) {
+            node.visited = false;
+            node.distance = Infinity;
+            node.parent = null;
+        }
+    }
+
+    // Save the calculated route from the space node to the source node (the employee)
+    saveEmployeeRoute(node: Node): void {
+        const employeeRoute: Route = new Array();
+        let currentNode: Node = node;
+        while (currentNode && currentNode.type !== NodeType.EMPLOYEE) {
+            employeeRoute.push(currentNode.parent.index);
+            currentNode = currentNode.parent;
+        }
+        node.employeesRoutes.push(employeeRoute);
     }
 }
